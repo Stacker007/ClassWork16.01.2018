1a191a1feee151d6fdd084bd49d7caedf75787ac
Vladimir <34865131+Stacker007@users.noreply.github.com>
2018-01-16 20:01:27 +0300

    Create Task2.cpp

---
+++ Task2.cpp
@@ -0,0 +1,49 @@
+/*
+2. Написать программу, выполняющую сортировку одномерного массива целых
+чисел по возрастанию методом пузырька. Направление просмотров – слева
+направо, минимизировать число просмотров.
+*/
+
+#include <iostream>
+#include <ctime>
+#define SIZE 10
+
+using namespace std;
+// ФУНКЦИЯ ДЛЯ ВЫВОДА МАССИВА :
+void printOurArr(int a[SIZE]) {
+	for (int i = 0; i < SIZE; i++) 
+		cout << a[i] << " ";	
+	cout << endl;
+}
+
+void main() {
+	setlocale(LC_ALL, "rus");
+	srand(time(0));
+	int ourArr[SIZE];
+	int countOfNull = 0;
+	int k = SIZE-1;
+	bool flag = true;
+	for (int i = 0; i < SIZE; i++)
+		ourArr[i] = rand() % 202 - 100; //Инициализируем массив
+	cout << "Исходный массив:\n";
+	printOurArr(ourArr); // Вызов функции вывода массива
+	while (k > 0 && flag)// к - левая граница подмассива
+	{
+		flag = false;
+		for (int i = 0; i < k; i++) 
+			if (ourArr[i] > ourArr[i + 1]) {
+				 //Находим максимальный элемент в части массива от k до SIZE 
+				int tmp = ourArr[i];
+				ourArr[i] = ourArr[i+1];
+				ourArr[i+1] = tmp;
+				flag = true;
+			}
+		k--;
+		
+	}
+	
+	cout << "Итоговый массив:\n";
+	printOurArr(ourArr); // Вызов функции вывода массива
+
+	system("pause");
+}
